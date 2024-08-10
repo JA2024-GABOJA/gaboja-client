d9172911ee03a98eec61cf320ce4701a5d931075
wade3420 <eamon3481@naver.com>
2024-08-11 01:09:05 +0900

    [config] 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    jsxImportSource: '@emotion/react',
-  })]
-})
-
-
+  resolve: {
+    alias: [
+      { find: '@components', replacement: '/src/components' },
+      { find: '@', replacement: '/src' },
+    ],
+  },
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+  ],
+});
